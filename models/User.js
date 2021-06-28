8c9e812a78dff7439794f0b4416abffd0f4e6399
jjjjonathan <hornathanjon@gmail.com>
2021-06-27 23:32:39 -0500

    add User model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  friendRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+// this should avoid overwrite warning error
+mongoose.models = {};
+
+export default mongoose.model('User', UserSchema);
