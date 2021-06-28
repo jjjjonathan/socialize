dddd307d163835be0d818b5f49069a92360924ad
jjjjonathan <hornathanjon@gmail.com>
2021-06-27 21:38:35 -0500

    add mongo middleware

---
+++ middleware/mongo.js
@@ -0,0 +1,20 @@
+// thanks to https://dev.to/raphaelchaula/adding-mongodb-mongoose-to-next-js-apis-3af
+
+import mongoose from 'mongoose';
+
+const connectDB = (handler) => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handler(req, res);
+  }
+  // Use new db connection
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  });
+  return handler(req, res);
+};
+
+export default connectDB;
