2b336966ba986b14b53481576aac258c16939726
jjjjonathan <hornathanjon@gmail.com>
2021-06-28 19:34:38 -0500

    add login styles

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,43 @@
 import Splash from '../components/Splash';
 import { Form, Button } from 'react-bootstrap';
+import styles from './login.module.css';
 
 const Login = () => {
   return (
     <Splash pageTitle="Log In">
-      <h1 className="logo">socialize</h1>
-      <Form>
-        <Form.Group controlId="username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" />
-        </Form.Group>
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button type="submit">Log In</Button>
-      </Form>
+      <div className={styles.form}>
+        <h1 className="logo text-center">socialize</h1>
+        <Form className="mt-5">
+          <Form.Group controlId="username" className="mb-2">
+            <Form.Label>
+              <small>Username</small>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Enter username" />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>
+              <small>Password</small>
+            </Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button
+            type="submit"
+            variant="outline-dark"
+            className={styles.button}
+          >
+            Log in
+          </Button>
+        </Form>
+        <Button variant="outline-dark" className={`mt-5 ${styles.button}`}>
+          Log in with Facebook
+        </Button>
+        <Button variant="outline-dark" className={`mt-2 ${styles.button}`}>
+          Log in with Google
+        </Button>
+        <Button variant="outline-dark" className={`mt-2 ${styles.button}`}>
+          Sign up with Email
+        </Button>
+      </div>
     </Splash>
   );
 };
