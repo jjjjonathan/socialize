1dab721af39b57e61686449c299af7fe4715c704
jjjjonathan <hornathanjon@gmail.com>
2021-06-28 23:55:21 -0500

    add color prop to circle spinner

--- components/CircleSpinner.js
+++ components/CircleSpinner.js
@@ -1,7 +1,10 @@
 import styles from './CircleSpinner.module.css';
 
-const CircleSpinner = () => (
-  <div className={styles.skChase}>
+const CircleSpinner = ({ color }) => (
+  <div
+    className={styles.skChase}
+    style={{ '--dot-color': `var(--${color || 'dark'})` }}
+  >
     <div className={styles.skChaseDot}></div>
     <div className={styles.skChaseDot}></div>
     <div className={styles.skChaseDot}></div>
