5e3cae18ef9e65466e9a3b52fe34f88cf17dcf90
jjjjonathan <hornathanjon@gmail.com>
2021-06-28 20:52:16 -0500

    add swr config and fetcher util

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { SWRConfig } from 'swr';
+import fetcher from '../utils/fetcher';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const swrOptions = {
+    fetcher,
+  };
+
+  return (
+    <SWRConfig value={swrOptions}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
 export default MyApp;
