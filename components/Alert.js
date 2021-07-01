bc5f9f55f4f9d26339db757e2acf30e8bee4fb01
jjjjonathan <hornathanjon@gmail.com>
2021-07-01 01:38:35 -0500

    fix alert bottom margin

--- components/Alert.js
+++ components/Alert.js
@@ -8,7 +8,7 @@ const Alert = ({ type, children }) => {
   return (
     <BootstrapAlert className="soft-alert">
       <BootstrapAlert.Heading as="h5">{heading()}</BootstrapAlert.Heading>
-      <p>{children}</p>
+      <p className="mb-0">{children}</p>
     </BootstrapAlert>
   );
 };
