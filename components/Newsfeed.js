81ffbd1a364e6ff73198367c6efc0e3a0cd27deb
jjjjonathan <hornathanjon@gmail.com>
2021-06-30 20:08:11 -0500

    add more to card

--- components/Newsfeed.js
+++ components/Newsfeed.js
@@ -7,7 +7,9 @@ const Newsfeed = () => (
       <Card.Body>
         Hey, my depression is a little better today! I{"'"}m feeling okay about
         myself!
+        <Card.Img variant="bottom" src="https://via.placeholder.com/150" />
       </Card.Body>
+      <Card.Footer>2 Likes</Card.Footer>
     </Card>
   </>
 );
