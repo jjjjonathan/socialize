d21a606c12ac513bfab5bca65a0465a46ac5e65a
jjjjonathan <hornathanjon@gmail.com>
2021-07-03 02:09:03 -0500

    add facebook auth route

---
+++ pages/api/auth/facebook/index.js
@@ -0,0 +1,10 @@
+import nc from 'next-connect';
+import middleware from '../../../../middleware';
+import passport from '../../../../middleware/passport';
+
+const handler = nc();
+handler.use(middleware);
+
+handler.get(passport.authenticate('facebook'));
+
+export default handler;
