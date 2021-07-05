6f246f550e58a4a651aff95656d10c656cb08d35
jjjjonathan <hornathanjon@gmail.com>
2021-07-04 22:18:09 -0500

    add module styles and profile pic to postcard

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,8 +1,20 @@
 import { Card } from 'react-bootstrap';
+import Image from 'next/image';
+import styles from './PostCard.module.css';
 
 const PostCard = ({ post }) => (
   <Card className="glass-card">
-    <Card.Header>{post.user.name}</Card.Header>
+    <Card.Header>
+      <div className="d-flex">
+        <Image
+          src={post.user.profilePicture}
+          height="100"
+          width="100"
+          alt={`Profile picture of ${post.user.name}`}
+        />
+        <h4 className={`h6 ${styles.name}`}>{post.user.name}</h4>
+      </div>
+    </Card.Header>
     <Card.Body>
       {post.body}
       {/* <Card.Img variant="bottom" src="https://via.placeholder.com/150" /> */}
