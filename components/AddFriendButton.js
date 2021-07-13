c99a87af5672f65a42af0757e30522088cb35932
jjjjonathan <hornathanjon@gmail.com>
2021-07-13 16:17:42 -0500

    rework add friend button

--- components/AddFriendButton.js
+++ components/AddFriendButton.js
@@ -1,33 +1,67 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import styles from './AddFriendButton.module.css';
+import CircleSpinner from './CircleSpinner';
 
-const AddFriendButton = ({ username, variant, className, ...props }) => {
-  const onClick = () => {
-    console.log(username);
+const AddFriendButton = ({ username, variant, ...props }) => {
+  const [status, setStatus] = useState('default');
+
+  const onClick = async () => {
+    try {
+      setStatus('submitting');
+      // await axios.post(`/api/user/${username}/add-friend`);
+      setTimeout(() => {
+        setStatus('success');
+      }, 2000);
+    } catch (error) {
+      // TODO add error message
+      console.error(error);
+    }
   };
 
   if (variant === 'mini') {
+    const innards = () => {
+      switch (status) {
+        case 'submitting':
+          return <CircleSpinner size="25" color="secondary" />;
+        case 'success':
+          return <i className="bi bi-check-lg text-secondary"></i>;
+        default:
+          return (
+            <Button
+              onClick={onClick}
+              variant="outline-secondary"
+              className={styles.mini}
+            >
+              <i className="bi bi-person-plus-fill"></i>
+            </Button>
+          );
+      }
+    };
+
     return (
-      <Button
-        onClick={onClick}
-        variant="outline-secondary"
-        className={`${className} ${styles.mini}`}
+      <div
         {...props}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 40,
+          height: 40,
+        }}
       >
-        <i className="bi bi-person-plus-fill"></i>
-      </Button>
+        {innards()}
+      </div>
     );
   }
 
   return (
-    <Button
-      onClick={onClick}
-      variant="secondary"
-      className={`${className} ${styles.large}`}
-      {...props}
-    >
-      <i className="bi bi-person-plus"></i> Add Friend
-    </Button>
+    <div {...props}>
+      <Button onClick={onClick} variant="secondary" className={styles.large}>
+        <i className="bi bi-person-plus"></i> Add Friend
+      </Button>
+    </div>
   );
 };
 
