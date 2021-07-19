c4faaaba533776ec7815020f309975eb1dbb4553
jjjjonathan <hornathanjon@gmail.com>
2021-07-19 18:13:42 -0500

    add newsfeed route to backend

---
+++ pages/api/user/newsfeed.js
@@ -0,0 +1,27 @@
+import nc from 'next-connect';
+import middleware from '../../../middleware';
+import User from '../../../models/User';
+import Post from '../../../models/Post';
+
+const handler = nc();
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  if (!req.user) return res.status(401).end();
+
+  const { id } = req.user;
+  const { friends } = await User.findById(id, 'friends');
+  const friendIds = friends.map((friend) => friend.user);
+
+  const posts = await Post.find({
+    user: {
+      $in: friendIds,
+    },
+  })
+    .populate('user', 'name username profilePicture')
+    .sort('-timestamp');
+
+  return res.json(posts);
+});
+
+export default handler;
