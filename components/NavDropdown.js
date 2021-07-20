7a1fbcb4c172355df20c2d21aa0b87940c599a49
jjjjonathan <hornathanjon@gmail.com>
2021-07-20 17:06:11 -0500

    add navigation buttons to test navbar

---
+++ components/NavDropdown.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import { Dropdown, Button } from 'react-bootstrap';
+import styles from './NavDropdown.module.css';
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <Button
+    className={styles.button}
+    variant="outline-dark"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {children}
+  </Button>
+));
+
+const NavDropdown = ({ children, icon, id, ...props }) => (
+  <Dropdown {...props}>
+    <Dropdown.Toggle as={CustomToggle} id={id}>
+      <i className={`bi ${icon}`}></i>
+    </Dropdown.Toggle>
+    <Dropdown.Menu align="right">{children}</Dropdown.Menu>
+  </Dropdown>
+);
+
+export default NavDropdown;
--- pages/test.js
+++ pages/test.js
@@ -1,4 +1,9 @@
+import { Navbar, Nav, Container, Dropdown } from 'react-bootstrap';
+import Link from 'next/link';
 import FriendRequest from '../components/FriendRequest';
+import FriendRequestsDropdown from '../components/FriendRequestsDropdown';
+import NavMenu from '../components/NavMenu';
+import NavDropdown from '../components/NavDropdown';
 
 const friendReq = {
   user: {
@@ -11,18 +16,70 @@ const friendReq = {
   id: '60ea2515bbe1b73602fec25d',
 };
 
+const currentUser = { name: 'Jonny Baby', username: 'jonnybaby12345' };
+
+const TestNavMenu = () => (
+  <Navbar bg="primary" expand="sm" fixed="top">
+    <Container>
+      <Link href="/" passHref>
+        <Navbar.Brand>socialize</Navbar.Brand>
+      </Link>
+
+      <NavDropdown
+        className="ml-auto"
+        icon="bi-person-lines-fill"
+        id="friend-requests-dropdown"
+      >
+        <p>Content</p>
+      </NavDropdown>
+
+      <NavDropdown
+        className="ml-4"
+        icon="bi-bell-fill"
+        id="notifications-dropdown"
+      >
+        <p>Content</p>
+      </NavDropdown>
+
+      <NavDropdown
+        className="ml-4"
+        icon="bi-three-dots"
+        id="hamburger-dropdown"
+      >
+        <Link href={`/settings`} passHref>
+          <Dropdown.Item>Settings</Dropdown.Item>
+        </Link>
+      </NavDropdown>
+
+      {/* <Nav className="ml-auto">
+        <FriendRequestsDropdown />
+        <NavDropdown title={`Welcome, ${currentUser.name}`} id="nav-dropdown">
+          <Link href={`/profile/${currentUser.username}`} passHref>
+            <NavDropdown.Item>Profile</NavDropdown.Item>
+          </Link>
+          <NavDropdown.Divider />
+          <NavDropdown.Item>Log out</NavDropdown.Item>
+        </NavDropdown>
+      </Nav> */}
+    </Container>
+  </Navbar>
+);
+
 const Test = () => (
-  <div
-    className="d-flex justify-content-center align-items-center"
-    style={{ height: '100vh' }}
-  >
-    <div style={{ background: 'lightgray', width: 250, height: 120 }}>
-      <FriendRequest friendReq={friendReq} />
+  <>
+    <TestNavMenu currentUser={currentUser} />
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: '100vh' }}
+    >
+      <div style={{ background: 'lightgray', width: 250, height: 120 }}>
+        <FriendRequest friendReq={friendReq} />
+      </div>
+      <div>Hello</div>
+      <div className="medium">Hello</div>
+      <div className="small">Hello</div>
     </div>
-    <div>Hello</div>
-    <div className="medium">Hello</div>
-    <div className="small">Hello</div>
-  </div>
+  </>
 );
 
 export default Test;
