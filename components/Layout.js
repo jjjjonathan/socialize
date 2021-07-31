a5f7bcb778857a03ccf45d3f0a4f36cdc603bb1a
jjjjonathan <hornathanjon@gmail.com>
2021-07-31 16:17:53 -0500

    switch to react hot toast

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Container } from 'react-bootstrap';
+import { Toaster } from 'react-hot-toast';
 import NavMenu from './NavMenu';
 import styles from './Layout.module.css';
 
@@ -14,6 +15,7 @@ const Layout = ({ pageTitle, children, currentUser }) => (
     <main className={styles.mainContainer}>
       <Container>{children}</Container>
     </main>
+    <Toaster position="bottom-left" />
   </>
 );
 
