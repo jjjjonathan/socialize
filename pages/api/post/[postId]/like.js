22f5ebfcc49b7bfc98a94d39a195c251acda721b
jjjjonathan <hornathanjon@gmail.com>
2021-08-01 18:53:42 -0500

    add like unlike to router

--- pages/api/post/[postId]/like.js
+++ pages/api/post/[postId]/like.js
@@ -1,5 +1,6 @@
 import nc from 'next-connect';
 import middleware from '../../../../middleware';
+import Post from '../../../../models/Post';
 
 const handler = nc();
 handler.use(middleware);
@@ -8,8 +9,23 @@ handler.post(async (req, res) => {
   if (!req.user) return res.status(401).end();
 
   const { postId } = req.query;
+  const userId = req.user.id;
 
-  return res.json({ postId });
+  const post = await Post.findById(postId);
+
+  const likeIndex = post.likes.findIndex(
+    (like) => like.toString() === userId.toString(),
+  );
+
+  if (likeIndex !== -1) {
+    post.likes.splice(likeIndex, 1);
+  } else {
+    post.likes.push(userId);
+  }
+
+  await post.save();
+
+  return res.json(post);
 });
 
 export default handler;
