b47b29b2beff8ea2f346f09ab2ab2669dab40ca3
jjjjonathan <hornathanjon@gmail.com>
2021-08-05 17:46:00 -0500

    finish styling likes modal

--- components/LikesModal.js
+++ components/LikesModal.js
@@ -1,7 +1,10 @@
 import { Modal } from 'react-bootstrap';
+import Link from 'next/link';
+import Image from './Image';
 import useLikes from '../hooks/useLikes';
 import CircleSpinner from './CircleSpinner';
 import FlatAlert from './FlatAlert';
+import styles from './LikesModal.module.css';
 
 const LikesModal = ({ postId, setShow, show }) => {
   const { likes, isLikesError, isLikesLoading } = useLikes(postId);
@@ -15,7 +18,21 @@ const LikesModal = ({ postId, setShow, show }) => {
     return (
       <ul>
         {likes.map((like) => (
-          <li key={like.id}>{like.name}</li>
+          <div className="d-flex align-items-center mb-2" key={like.id}>
+            <Image
+              publicId={like.profilePicture}
+              size="40"
+              variant="circle"
+              profilePicName={like.name}
+              href={`/profile/${like.username}`}
+              layout="fixed"
+            />
+            <Link href={`/profile/${like.username}`} passHref>
+              <a className={`h6 ml-3 mb-1 text-secondary ${styles.name}`}>
+                {like.name}
+              </a>
+            </Link>
+          </div>
         ))}
       </ul>
     );
@@ -23,7 +40,9 @@ const LikesModal = ({ postId, setShow, show }) => {
 
   return (
     <Modal show={show} onHide={() => setShow(false)}>
-      <Modal.Header closeButton />
+      <Modal.Header closeButton>
+        <Modal.Title>Likes</Modal.Title>
+      </Modal.Header>
       <Modal.Body>{innards()}</Modal.Body>
     </Modal>
   );
