6ac63aaa5676ef24b19289de046f52a45c06f66a
jjjjonathan <hornathanjon@gmail.com>
2021-08-07 12:54:23 -0500

    repurpose new post form to about me update

--- components/AboutMeUpdate.js
+++ components/AboutMeUpdate.js
@@ -1,3 +1,78 @@
-const AboutMeUpdate = () => <div>Update about me</div>;
+import { Card, Form, Button } from 'react-bootstrap';
+import { Formik, Form as FormikForm } from 'formik';
+import TextareaAutosize from 'react-textarea-autosize';
+import * as yup from 'yup';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import FlatSpinner from './FlatSpinner';
+
+const AboutMeUpdate = () => {
+  const handleUpdateBio = async ({ bio }) => {
+    try {
+      await axios.post('/api/user/bio', { bio });
+
+      toast.success('Successfully updated About Me!');
+    } catch (error) {
+      console.error(error);
+      toast.error('Could not update About Me!');
+    }
+  };
+
+  const validationSchema = yup.object().shape({
+    bio: yup.string().label('About me').min(3).max(1000),
+  });
+
+  return (
+    <Formik
+      initialValues={{ bio: '' }}
+      validationSchema={validationSchema}
+      onSubmit={handleUpdateBio}
+    >
+      {({ isSubmitting, errors, touched, values, handleChange }) => (
+        <>
+          <Card className="glass-card-form mb-4">
+            <Card.Body>
+              <FormikForm noValidate>
+                <Form.Control
+                  as={TextareaAutosize}
+                  minRows={3}
+                  maxRows={12}
+                  className={`glass-card-textarea ${
+                    errors.newPost && touched.newPost ? 'is-invalid' : null
+                  }`}
+                  placeholder="About Me"
+                  name="bio"
+                  id="bio"
+                  value={values.bio}
+                  onChange={handleChange}
+                />
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="new-post-submit-button"
+                  style={{ width: 38, height: 24, boxSizing: 'content-box' }}
+                >
+                  {isSubmitting ? (
+                    <FlatSpinner
+                      size="18"
+                      style={{ marginTop: '-3px', marginLeft: '-8px' }}
+                    />
+                  ) : (
+                    'Update'
+                  )}
+                </Button>
+              </FormikForm>
+            </Card.Body>
+          </Card>
+          {errors.bio && touched.bio ? (
+            <div className="invalid-feedback new-post-validation-error">
+              {errors.bio}
+            </div>
+          ) : null}
+        </>
+      )}
+    </Formik>
+  );
+};
 
 export default AboutMeUpdate;
