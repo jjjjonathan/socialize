9422cd19665da9cf30a9517d13d3bac64e178e38
jjjjonathan <hornathanjon@gmail.com>
2021-08-13 17:37:29 -0500

    add email test route

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,15 +1,22 @@
 import nc from 'next-connect';
 import middleware from '../../middleware';
-import User from '../../models/User';
+import sendEmail from '../../utils/sendEmail';
 
 const handler = nc();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
-  const users = await User.find({}, 'username requestedFriends').populate(
-    'requestedFriends',
-  );
-  return res.json(users);
+  const email = {
+    to: 'hornathanjon@gmail.com',
+    from: 'jonathanhorn000@gmail.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+
+  await sendEmail(email);
+
+  res.status(204).end();
 });
 
 export default handler;
