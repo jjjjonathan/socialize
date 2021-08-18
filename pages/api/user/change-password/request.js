020175ce6da65c21312ba89e0ae37ba1928dc513
jjjjonathan <hornathanjon@gmail.com>
2021-08-18 16:09:07 -0500

    modify request password change to work with email in req body

--- pages/api/user/change-password/request.js
+++ pages/api/user/change-password/request.js
@@ -3,12 +3,28 @@ import { nanoid } from 'nanoid';
 import middleware from '../../../../middleware';
 import Token from '../../../../models/Token';
 import sendEmail from '../../../../utils/sendEmail';
+import User from '../../../../models/User';
 
 const handler = nc();
 handler.use(middleware);
 
 handler.post(async (req, res) => {
-  if (!req.user) return res.status(401).end();
+  if (!req.user && !req.body?.email) return res.status(401).end();
+
+  let email;
+  let userId;
+  let name;
+
+  if (req.body?.email) {
+    email = req.body.email;
+    const user = await User.findOne({ email });
+    userId = user.id;
+    name = user.name;
+  } else {
+    email = req.user.email;
+    userId = req.user.id;
+    name = req.user.name;
+  }
 
   // Create token for email verification
 
@@ -16,7 +32,7 @@ handler.post(async (req, res) => {
   const tokenObject = new Token({
     token: verificationToken,
     type: 'changePassword',
-    user: req.user.id,
+    user: userId,
   });
 
   await tokenObject.save();
@@ -26,13 +42,13 @@ handler.post(async (req, res) => {
   const verificationLink = `${process.env.WEB_URI}/change-password/${verificationToken}`;
 
   await sendEmail({
-    to: req.user.email,
+    to: email,
     from: { name: 'Jonathan at Socialize', email: process.env.EMAIL_FROM },
     subject: 'Password reset request',
-    text: `Hello, ${req.user.name}! Please follow this link to reset your password: ${verificationLink} (Link will expire in 20 minutes)`,
+    text: `Hello, ${name}! Please follow this link to reset your password: ${verificationLink} (Link will expire in 20 minutes)`,
     html: `
     <div>
-      <h3>Hello, ${req.user.name}!</h3>
+      <h3>Hello, ${name}!</h3>
       <p>Please follow the link below to reset your password:</p>
       <p><a href="${verificationLink}">${verificationLink}</a></p>
       <p>The above link will expire in 20 minutes.</p>
