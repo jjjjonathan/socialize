844d3d10a84c899c3f49d1ffd09fdc417157ce14
jjjjonathan <hornathanjon@gmail.com>
2021-08-19 17:27:52 -0500

    remove morgan from project

--- middleware/index.js
+++ middleware/index.js
@@ -1,25 +1,9 @@
 import nc from 'next-connect';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import morgan from 'morgan';
-
 import mongo from './mongo';
 import passport from './passport';
 import session from './session';
 
-// eslint-disable-next-line no-unused-vars
-function onError(err, req, res, _next) {
-  console.log('\n\n------ Middleware caught error: ------\n');
-  console.log(err);
-  console.log('\n--------------------------------------\n\n');
-
-  res.status(500).end(err.toString());
-  // OR: you may want to continue
-  // next();
-}
-
-const router = nc({ onError });
-
-router.use(morgan('dev'));
+const router = nc();
 
 router.use(mongo);
 
