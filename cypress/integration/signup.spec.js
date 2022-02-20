b438049be6046b934a50161f7b84910eaeb655f4
jjjjonathan <hornathanjon@gmail.com>
2022-02-20 17:14:45 -0600

    Progress on sign up spec

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -7,11 +7,23 @@ describe('Sign up', () => {
     cy.location('pathname').should('equal', '/signup');
   });
 
-  beforeEach(() => {
-    cy.visit('/signup');
-  });
-
   it('works in UI with valid info', () => {
-    // TODO fill this in
+    cy.visit('/signup');
+
+    // Fill username and password fields and click login button
+    cy.get('[name=name]').type(Cypress.env('name'));
+    cy.get('[name=username]').type(Cypress.env('username'));
+    cy.get('[name=email]').type(Cypress.env('email'));
+    cy.get('[name=password]').type(Cypress.env('password'));
+    cy.get('[name=passwordConf]').type(Cypress.env('password'));
+    cy.contains('button', 'Sign up').click();
+
+    // Should display confirmation email screen
+    cy.location('pathname').should('equal', '/verify-email');
+    // TODO add visual confirmation
+
+    // Ping verify email test API route
+
+    // Login
   });
 });
