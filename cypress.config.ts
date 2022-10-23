b4c2485fd8253a9fbcb596f98df3a0d8c5e8f3f9
jjjjonathan <hornathanjon@gmail.com>
2022-10-22 19:33:38 -0700

    install cypress

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
