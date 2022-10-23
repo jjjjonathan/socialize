644d944bcbe989a7eaee3c06dd85bcbde80ede96
jjjjonathan <hornathanjon@gmail.com>
2022-10-22 20:08:03 -0700

    cy config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,14 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:3333',
+    video: false,
+    screenshotOnRunFailure: false,
+    env: {
+      name: 'Cypress Tester',
+      username: 'cypresstester',
+      email: 'example@example.com',
+      password: 'password',
     },
   },
 });
