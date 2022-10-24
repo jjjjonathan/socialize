af21edd2feeb62938979cd74322d934fa6e20b42
jjjjonathan <hornathanjon@gmail.com>
2022-10-23 18:36:40 -0700

    Sign up tests

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,31 +1,95 @@
 describe('Sign up page', () => {
-  it('is navigable by home page link', () => {
-    cy.visit('/');
-    cy.location('pathname').should('equal', '/login');
+  context('Navigation', () => {
+    it('is navigable via home page link', () => {
+      cy.visit('/');
+      cy.location('pathname').should('equal', '/login');
 
-    cy.contains('Sign up').click();
-    cy.location('pathname').should('equal', '/signup');
+      cy.contains('Sign up').click();
+      cy.location('pathname').should('equal', '/signup');
+    });
   });
 
-  it.skip('UI works with valid info', () => {
-    cy.visit('/signup');
+  context('Functionality', () => {
+    beforeEach(() => {
+      cy.visit('/signup');
+    });
 
-    // Fill username and password fields and click login button
-    cy.get('[name=name]').type(Cypress.env('name'));
-    cy.get('[name=username]').type(Cypress.env('username'));
-    cy.get('[name=email]').type(Cypress.env('email'));
-    cy.get('[name=password]').type(Cypress.env('password'));
-    cy.get('[name=passwordConf]').type(Cypress.env('password'));
-    cy.contains('button', 'Sign up').click();
+    it('UI works with valid info', () => {
+      cy.exec('npm run seed');
 
-    // Should display confirmation email screen
-    cy.location('pathname').should('equal', '/verify-email');
-    // TODO add visual confirmation
+      // Fill username and password fields and click login button
+      cy.get('[name=name]').type(Cypress.env('name'));
+      cy.get('[name=username]').type(Cypress.env('username'));
+      cy.get('[name=email]').type(Cypress.env('email'));
+      cy.get('[name=password]').type(Cypress.env('password'));
+      cy.get('[name=passwordConf]').type(Cypress.env('password'));
+      cy.contains('button', 'Sign up').click();
 
-    // Ping verify email test API route
+      cy.location('pathname').should('equal', '/verify-email');
+    });
 
-    // Login
+    it('displays error message if email is invalid', () => {
+      // Fill with invalid email address
+      cy.get('[name=name]').type(Cypress.env('name'));
+      cy.get('[name=username]').type(Cypress.env('username'));
+      cy.get('[name=email]').type('bademail@.');
+      cy.get('[name=password]').type(Cypress.env('password'));
+      cy.get('[name=passwordConf]').type(Cypress.env('password'));
+      cy.contains('button', 'Sign up').click();
+
+      cy.contains('Email address must be a valid email').should('exist');
+    });
+
+    it('displays error message if email is already in use', () => {
+      cy.exec('npm run seed');
+
+      // Fill with existing email address
+      cy.get('[name=name]').type(Cypress.env('name'));
+      cy.get('[name=username]').type(Cypress.env('username'));
+      cy.get('[name=email]').type('example@example.com');
+      cy.get('[name=password]').type(Cypress.env('password'));
+      cy.get('[name=passwordConf]').type(Cypress.env('password'));
+      cy.contains('button', 'Sign up').click();
+
+      cy.contains('User already exists with email address').should('exist');
+    });
+
+    it('displays error message if username is already in use', () => {
+      cy.exec('npm run seed');
+
+      // Fill with existing username
+      cy.get('[name=name]').type(Cypress.env('name'));
+      cy.get('[name=username]').type('seedperson');
+      cy.get('[name=email]').type(Cypress.env('email'));
+      cy.get('[name=password]').type(Cypress.env('password'));
+      cy.get('[name=passwordConf]').type(Cypress.env('password'));
+      cy.contains('button', 'Sign up').click();
+
+      cy.contains('Username seedperson is already in use').should('exist');
+    });
+
+    it("displays error message if password doesn't meet requirements", () => {
+      // Fill with password that's too short
+      cy.get('[name=name]').type(Cypress.env('name'));
+      cy.get('[name=username]').type(Cypress.env('username'));
+      cy.get('[name=email]').type(Cypress.env('email'));
+      cy.get('[name=password]').type('123');
+      cy.get('[name=passwordConf]').type('123');
+      cy.contains('button', 'Sign up').click();
+
+      cy.contains('Password must be at least 8 characters').should('exist');
+    });
+
+    it("displays error message if passwords don't match", () => {
+      // Fill with unmatching passwords
+      cy.get('[name=name]').type(Cypress.env('name'));
+      cy.get('[name=username]').type(Cypress.env('username'));
+      cy.get('[name=email]').type(Cypress.env('email'));
+      cy.get('[name=password]').type('12345678');
+      cy.get('[name=passwordConf]').type('password');
+      cy.contains('button', 'Sign up').click();
+
+      cy.contains('Passwords must match').should('exist');
+    });
   });
-
-  it('is currently unimplemented');
 });
